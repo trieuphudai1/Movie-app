cff6573aa5ad9cf4ad1f332f2960f848d7c4ea89
Phuz <trieuphudai190@gmail.com>
2025-10-25 02:38:44 +0700

    bg of movie app

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <main>
+            <div className="pattern" />
+            <div className="wrapper">
+                <header>
+                    <img src="./hero.png" alt="Hero Banner"/>
+                    <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without Hassle</h1>
+                </header>
+            </div>
+        </main>
+    );
+};
+
+export default App;
