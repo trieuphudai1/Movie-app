d0f5c3815a5ebe999efdff819662703a11b2a639
Phuz <trieuphudai190@gmail.com>
2025-10-25 13:32:15 +0700

    Add search component

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Search from "./components/Search.jsx";
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+
     return (
         <main>
             <div className="pattern" />
@@ -9,6 +13,8 @@ const App = () => {
                     <img src="./hero.png" alt="Hero Banner"/>
                     <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without Hassle</h1>
                 </header>
+
+                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
             </div>
         </main>
     );
